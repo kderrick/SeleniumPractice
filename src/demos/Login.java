219e51302e3b650aef6286f23d82445028988270
Kyle <kderrick1988@gmail.com>
2018-03-28 19:31:25 -0700

    open chrome with driver

--- src/demos/Login.java
+++ src/demos/Login.java
@@ -5,8 +5,11 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Login {
 	public static void main(String[] args) {
-		System.setProperty("webdriver.chrome.driver", "/Users/kylederrick/Desktop/Software/chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", "/Users/kylederrick/Desktop/Software/chromedriver");
 		WebDriver driver = new ChromeDriver();
+		
+		
+		driver.get("https://www.google.com/");
 	}
 
 }
