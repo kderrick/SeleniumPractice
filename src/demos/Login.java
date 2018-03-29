5663e48554649d1168f4b6fdd830a3c5f273b642
Kyle <kderrick1988@gmail.com>
2018-03-28 19:19:35 -0700

    instantiate ChromeDriver

--- src/demos/Login.java
+++ src/demos/Login.java
@@ -1,5 +1,12 @@
 package demos;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Login {
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "/Users/kylederrick/Desktop/Software/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+	}
 
 }
