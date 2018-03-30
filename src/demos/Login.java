6a8ee36fe3f50312e3e1f38ba5caa6a7f5679d94
Kyle <kderrick1988@gmail.com>
2018-03-29 18:28:49 -0700

    add getText from website

--- src/demos/Login.java
+++ src/demos/Login.java
@@ -13,6 +13,8 @@ public class Login {
 		driver.get("https://www.google.com/");
 		driver.findElement(By.id("lst-ib")).sendKeys("Udemy");
 		driver.findElement(By.name("btnI")).click();
+		String confirm = driver.findElement(By.className("notice-streamer__headline")).getText();
+		System.out.println("CONFIRMATION:" + confirm);
 	}
 
 }
