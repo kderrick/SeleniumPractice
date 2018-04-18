e071dce21f5c57bfbcc107fc883dd1af639e20ac
Kyle <kderrick1988@gmail.com>
2018-04-17 20:58:22 -0700

    add setUp and tearDown methods

--- src/smoketests/PageTitleJUnit.java
+++ src/smoketests/PageTitleJUnit.java
@@ -1,17 +1,33 @@
 package smoketests;
 
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 
 public class PageTitleJUnit {
+	WebDriver driver;
+	String websiteURL = "http://sdettraining.com/trguitransactions/AccountManagement.aspx";
 
 	@Test
 	public void pageTitleTest() {
-		String websiteURL = "http://sdettraining.com/trguitransactions/AccountManagement.aspx";
-		
-		WebDriver driver = utilities.DriverFactory.open("chrome");
+		System.out.println("Starting the test");
 		driver.get(websiteURL);
 		String testTitle = driver.getTitle();
 		String ActualSiteTitle = "SDET Training | Account Management";
+		Assert.assertEquals(testTitle,ActualSiteTitle);
+	}
+	
+	@Before
+	public void setUp() {
+		System.out.println("Initialize the Driver");
+		driver = utilities.DriverFactory.open("chrome");
+	}
+	
+	@After
+	public void tearDown() {
+		System.out.println("Closing the driver");
+		driver.close();
 	}
 }
