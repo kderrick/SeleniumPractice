03781ce75f5e7134335056c7f98869c8f8f10595
Kyle <kderrick1988@gmail.com>
2018-04-19 16:01:24 -0700

    add assertions,setUp and tearDown methods

--- src/smoketests/LoginTestNG.java
+++ src/smoketests/LoginTestNG.java
@@ -2,9 +2,14 @@ package smoketests;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class LoginTestNG {
+	
+	WebDriver driver;
 	
 	@Test
 	public void loginElementsArePresent() {
@@ -13,6 +18,24 @@ public class LoginTestNG {
 		
 		boolean loginUserID = driver.findElement(By.id("MainContent_txtUserName")).isDisplayed();
 		boolean loginPassword = driver.findElement(By.id("MainContent_txtPassword")).isDisplayed();
+		
+		Assert.assertTrue(loginUserID, "UserID text box displayed");
+		Assert.assertTrue(loginPassword, "Password text box displayed");
+		
+	}
+	
+	@BeforeMethod
+	public void setUp() {
+		System.out.println("Setting up test");
+		String webURL = "http://sdettraining.com/trguitransactions/AccountManagement.aspx";
+		driver = utilities.DriverFactory.open("chrome");
+		driver.get(webURL);
+	}
+	
+	@AfterMethod
+	public void tearDown() {
+		System.out.println("Tearing down Test");
+		driver.close();
 	}
 
 }
