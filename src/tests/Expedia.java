1cb2afe0bf5a3fbb621cdbd73ee01c49beb3c9cb
Kyle <kderrick1988@gmail.com>
2018-05-03 16:41:43 -0700

    add steps to clock hotel, enter dates and click search

--- src/tests/Expedia.java
+++ src/tests/Expedia.java
@@ -1,6 +1,8 @@
 package tests;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -9,10 +11,22 @@ public class Expedia {
 	
 	WebDriver driver;
 	String browserType = "Chrome";
+	String city = "New York, New York";
+	String checkIn = "07/10/2018";
+	String checkOut = "07/12/2018";
 	
 	@Test
 	public void hotelReservation() {
+		driver.findElement(By.id("tab-hotel-tab-hp")).click();
+		driver.findElement(By.id("hotel-destination-hp-hotel")).sendKeys(city);
 		
+		driver.findElement(By.id("hotel-checkin-hp-hotel")).sendKeys(checkIn);
+		//driver.findElement(By.id("hotel-checkout-hp-hotel")).sendKeys(checkOut);
+		
+		driver.findElement(By.xpath("//*[@id='hotel-checkout-hp-hotel']")).click();
+	//	driver.findElement(By.xpath("//*[@id='gcw-hotel-form-hp-hotel']/div[4]/div[3]/div/ul/li/div/div/div/div[2]/div[4]/button/span[1]/svg")).click();
+		
+		driver.findElement(By.xpath("//*[@id='gcw-hotel-form-hp-hotel']/div[9]/label/button")).click();
 	}
 	
 	@BeforeMethod
@@ -23,6 +37,6 @@ public class Expedia {
 	
 	@AfterMethod
 	public void tearDown() {
-		driver.quit();
+		//driver.quit();
 	}
 }
