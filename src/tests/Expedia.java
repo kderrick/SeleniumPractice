79e97d6542301d15bbd116191be0632caff86caf
Kyle <kderrick1988@gmail.com>
2018-05-04 06:58:28 -0700

    Added implicit wait to setUp, cleared fields on search before entering data and modified returned search results

--- src/tests/Expedia.java
+++ src/tests/Expedia.java
@@ -1,5 +1,7 @@
 package tests;
 
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.Select;
@@ -18,21 +20,31 @@ public class Expedia {
 	@Test
 	public void hotelReservation() {
 		driver.findElement(By.id("tab-hotel-tab-hp")).click();
+		driver.findElement(By.id("hotel-destination-hp-hotel")).clear();
 		driver.findElement(By.id("hotel-destination-hp-hotel")).sendKeys(city);
 		
+		driver.findElement(By.id("hotel-checkin-hp-hotel")).clear();
 		driver.findElement(By.id("hotel-checkin-hp-hotel")).sendKeys(checkIn);
-		//driver.findElement(By.id("hotel-checkout-hp-hotel")).sendKeys(checkOut);
+		
+		driver.findElement(By.id("hotel-checkout-hp-hotel")).clear();
+		driver.findElement(By.id("hotel-checkout-hp-hotel")).sendKeys(checkOut);
 		
 		driver.findElement(By.xpath("//*[@id='hotel-checkout-hp-hotel']")).click();
-	//	driver.findElement(By.xpath("//*[@id='gcw-hotel-form-hp-hotel']/div[4]/div[3]/div/ul/li/div/div/div/div[2]/div[4]/button/span[1]/svg")).click();
+		//driver.findElement(By.xpath("//*[@id='gcw-hotel-form-hp-hotel']/div[4]/div[3]/div/ul/li/div/div/div/div[2]/div[4]/button/span[1]/svg")).click();
 		
 		driver.findElement(By.xpath("//*[@id='gcw-hotel-form-hp-hotel']/div[9]/label/button")).click();
+		
+		
+		driver.findElement(By.cssSelector("input[name='star'][value='40']")).click();
 	}
 	
 	@BeforeMethod
 	public void setUp() {
 		driver = utilities.DriverFactory.open(browserType);
+		driver.manage().timeouts().implicitlyWait(7, TimeUnit.SECONDS);
 		driver.get("https://www.expedia.com/");
+		//Added implicit wait to setUp, cleared fields on search before entering data and modified returned search retults
+		
 	}
 	
 	@AfterMethod
