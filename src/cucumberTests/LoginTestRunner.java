c53cbe18f50a0c0b57be54ad8ce67a56a897f780
Kyle <kderrick1988@gmail.com>
2018-05-22 20:40:23 -0700

    add LoginTestRunner utilizing cucumber

---
+++ src/cucumberTests/LoginTestRunner.java
@@ -0,0 +1,14 @@
+package cucumberTests;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features="features", glue="stepImplementations")
+public class LoginTestRunner {
+
+	
+	
+}
