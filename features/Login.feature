cad03d89dddee079e08406a67e65ed81a99ece0b
Kyle <kderrick1988@gmail.com>
2018-05-28 09:31:43 -0700

    WIP: BDDLoginTest has failures debugging

--- features/Login.feature
+++ features/Login.feature
@@ -6,15 +6,16 @@ Feature: Test Login functionality on sdet university
 #A feature may have given specific scenarios
 #Scenarios use given/when/then structures
 Scenario: The user should be able to login with correct user name and correct password
-Given user is on the sign in page
+Given user is on the login page
 When user enters correct username and correct password
-Then 
+Then user gets confirmation 
 
 Scenario Outline: The user should be able to login
-Given: The user is on the login page
-When: The user enters <username>
-And: The user enters <password>
-Then: The user gets confirmation
+Given user is on the login page
+When user enters email <username>
+And user enters password <password>
+And user clicks login button
+Then user gets confirmation
 
 
 Examples:
--- src/stepImplementations/BDDLoginTest.java
+++ src/stepImplementations/BDDLoginTest.java
@@ -4,6 +4,7 @@ import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
+import cucumber.api.java.After;
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
@@ -13,11 +14,11 @@ public class BDDLoginTest {
 	
 	WebDriver driver;
 	
-	@Given("^user is on the sign in page$")
-	public void user_is_on_the_sign_in_page() {
-		System.out.println("USer is on Login page");
+	@Given("^user is on the login page$")
+	public void user_is_on_the_login_page() {
+		System.out.println("User is on Login page");
 		
-		driver = utilities.DriverFactory.open("Chrome");
+		driver = utilities.DriverFactory.open("firefox");
 		driver.get("http://sdettraining.com/trguitransactions/AccountManagement.aspx");
 	}
 	
@@ -30,23 +31,34 @@ public class BDDLoginTest {
 		
 	}
 	
-	@When("^user enters (.*)$")
+	@When("^user enters email (.*)$")
 	public void user_enters_username(String username) {
 		System.out.println("TESTING: " + username);
 		driver.findElement(By.id("MainContent_txtUserName")).sendKeys(username);
 		
 	}
 	
-	@And("^user enters (.*)$")
+	@And("^user enters password (.*)$")
 	public void user_enters_password(String password) {
 		driver.findElement(By.id("MainContent_txtPassword")).sendKeys(password);
 	}
 	
+	@And("user clicks login button")
+	public void click_login() {
+		driver.findElement(By.id("MainContent_btnLogin")).click();
+	}
 	
-	@Then("^user gets the confirmation$")
+	
+	@Then("^user gets confirmation$")
 	public void user_gets_the_confirmation() {
 		System.out.println("User then gets the confirmation");
 		Assert.assertTrue(driver.findElement(By.id("conf_message")).getText().contains("success"));
+		System.out.println(driver.findElement(By.id("conf_message")).getText());
+	}
+	
+	@After
+	public void tearDown() {
+		driver.quit();
 	}
 
 }
